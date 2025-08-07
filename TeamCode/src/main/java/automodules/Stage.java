4e96f2d3cba97a67a3e540fe07df6af1d6463d29
anany <ananyamujoo@gmail.com>
2025-08-06 20:38:21 -0700

    finished stage

--- TeamCode/src/main/java/automodules/Stage.java
+++ TeamCode/src/main/java/automodules/Stage.java
@@ -8,126 +8,40 @@ import java.util.function.Supplier;
 import robotparts.RobotPart;
 
 public class Stage {
-
     private final Runnable setup;
     private final Runnable loop;
-    private final Runnable end;
     private final Supplier<Boolean> exitCondition;
+    /**Runs unless cancelled*/
+    private final Runnable wrapUp;
+    /**Runs regardless of cancel, use safe code*/
+    private final Runnable alwaysRunsAtEnd;
 
-    public Stage(Runnable setup, Runnable loop, Supplier<Boolean> exitCondition, Runnable end) {
+    private final AtomicBoolean cancelRequested;
+    public Stage(Runnable setup, Runnable loop, Supplier<Boolean> exitCondition, Runnable wrapUp, Runnable alwaysRunsAtEnd) {
         this.setup = setup;
         this.loop = loop;
+        this.wrapUp = wrapUp;
         this.exitCondition = exitCondition;
-        this.end = end;
-
-    }
-    //TODO it should just do a start, then while exit condition loop, and then we do a stop.
-    public void start(){
-        Iterator.forAll(components, StageComponent::start);
-        hasStarted = true;
+        this.alwaysRunsAtEnd = alwaysRunsAtEnd;
+        cancelRequested = new AtomicBoolean(false);
     }
 
-    public void loop(){
-        Iterator.forAll(components, StageComponent::loop);
-    }
-
-    public boolean shouldStop(){
-        return Iterator.forAllConditionOR(components, StageComponent::shouldStop);
-    }
-
-    public void runOnStop(){
-        Iterator.forAll(components, StageComponent::runOnStop);
-        hasStarted = false;
-    }
-
-    public boolean isPause(){
-        return isPause;
-    }
-
-    private void addDefaults(){
-        if(Iterator.forAllCount(components, comp -> comp instanceof Exit) == 0){
-            components.add(RobotPart.exitAlways());
+    public void runStage(){
+        cancelRequested.set(false);
+        setup.run();
+        while(!exitCondition.get() && !cancelRequested.get()){
+            loop.run();
         }
+        if(!cancelRequested.get()){
+            wrapUp.run();
+        }
+        alwaysRunsAtEnd.run();
+    }
+    public void cancel(){
+        cancelRequested.set(true);
     }
 
-    public Stage combine(Stage stage){
-        Iterator.forAll(stage.components, components::add);
-        return this;
-    }
+    //TODO it should just do a start, then while exit condition loop, and then we do a stop.
+    //TODO In automodule class, we need to make cancel so it never runs the next stages
 
-    public Stage combine(StageComponent... stageComponents){
-        components.addAll(Arrays.asList(stageComponents));
-        return this;
-    }
-
-    public Stage attach(Stage stage){
-        final ArrayList<StageComponent> oldComponents = new ArrayList<>(this.components);
-        final ArrayList<StageComponent> newComponents = new ArrayList<>(stage.components);
-        FinalInteger exitCode = new FinalInteger();
-        return new Stage(false, false){
-            @Override
-            public void start() {
-                Iterator.forAll(oldComponents, StageComponent::start);
-                Iterator.forAll(newComponents, StageComponent::start);
-                super.hasStarted = true;
-            }
-            @Override
-            public void loop() {
-                switch (exitCode.get()) {
-                    case 0:
-                        Iterator.forAll(oldComponents, StageComponent::loop);
-                        Iterator.forAll(newComponents, StageComponent::loop);
-                        boolean oldStop = Iterator.forAllConditionOR(oldComponents, StageComponent::shouldStop);
-                        boolean newStop = Iterator.forAllConditionOR(newComponents, StageComponent::shouldStop);
-                        if (oldStop && newStop) {
-                            Iterator.forAll(oldComponents, StageComponent::runOnStop);
-                            Iterator.forAll(newComponents, StageComponent::runOnStop);
-                            exitCode.set(3);
-                        } else if (oldStop) {
-                            Iterator.forAll(oldComponents, StageComponent::runOnStop);
-                            exitCode.set(1);
-                        } else if (newStop) {
-                            Iterator.forAll(newComponents, StageComponent::runOnStop);
-                            exitCode.set(2);
-                        }
-                        break;
-                    case 1:
-                        Iterator.forAll(newComponents, StageComponent::loop);
-                        if (Iterator.forAllConditionOR(newComponents, StageComponent::shouldStop)) {
-                            Iterator.forAll(newComponents, StageComponent::runOnStop);
-                            exitCode.set(3);
-                        }
-                        break;
-                    case 2:
-                        Iterator.forAll(oldComponents, StageComponent::loop);
-                        if (Iterator.forAllConditionOR(oldComponents, StageComponent::shouldStop)) {
-                            Iterator.forAll(oldComponents, StageComponent::runOnStop);
-                            exitCode.set(3);
-                        }
-                        break;
-                }
-            }
-            @Override
-            public boolean shouldStop(){
-                return exitCode.equals(3);
-            }
-            @Override
-            public void runOnStop() {
-                switch (exitCode.get()){
-                    case 0:
-                        Iterator.forAll(oldComponents, StageComponent::runOnStop);
-                        Iterator.forAll(newComponents, StageComponent::runOnStop);
-                        break;
-                    case 1:
-                        Iterator.forAll(newComponents, StageComponent::runOnStop);
-                        break;
-                    case 2:
-                        Iterator.forAll(oldComponents, StageComponent::runOnStop);
-                        break;
-                }
-                super.hasStarted = false;
-                exitCode.set(0);
-            }
-        };
-    }
 }
