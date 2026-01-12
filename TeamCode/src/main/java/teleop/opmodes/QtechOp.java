dfff7ad3a195aeb1ddd083a9c736aa24f040e4be
anany <ananyamujoo@gmail.com>
2026-01-11 19:39:19 -0800

    qtech first commit uwu

---
+++ TeamCode/src/main/java/teleop/opmodes/QtechOp.java
@@ -0,0 +1,31 @@
+package teleop.opmodes;
+
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+import teleop.Tele;
+
+@Disabled
+@TeleOp
+public class QtechOp extends Tele {
+
+
+
+
+    @Override
+    public void initTele() {
+
+//        gpA.onceMode(Button.B, Test);
+    }
+
+
+    @Override
+    public void loopTele() {
+
+//        display("A: Press Mode");
+//        drive.move(0.2,0.3,0.4);
+
+
+
+    }
+}
