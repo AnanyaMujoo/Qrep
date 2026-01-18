10898b9637d90733d1f4844a30c39a8e6c1a7796
anany <ananyamujoo@gmail.com>
2026-01-18 12:50:23 -0800

    zeropower

--- TeamCode/src/main/java/robotparts/hardware/Shooter.java
+++ TeamCode/src/main/java/robotparts/hardware/Shooter.java
@@ -91,4 +91,25 @@ public class Shooter extends RobotPart {
         }
         shoot(-manualPower);
     }
+
+    public void zeroPower() {
+        // 2. CHECK TIME: If less than 0.25 seconds have passed, ignore this click
+        if (System.currentTimeMillis() - lastPressTime < 250) {
+            return;
+        }
+        // Update the last press time
+        lastPressTime = System.currentTimeMillis();
+
+        // 3. set 0 POWER
+        manualPower = 0;
+
+        // 4. ROUND IT: This forces 0.700001 to become exactly 0.7
+        manualPower = Math.round(manualPower * 10.0) / 10.0;
+
+        // 5. WRAP AROUND
+        if (manualPower > 1.0) {
+            manualPower = 0;
+        }
+        shoot(manualPower);
+    }
 }
